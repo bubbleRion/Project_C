c73b675d9f6d981d7d14e0660b146b0c3416fd61
bubblerion <windowrains@naver.com>
2025-03-25 14:42:10 +0900

    secret path add

---
+++ server/routes/board.js
@@ -0,0 +1,6 @@
+const router = express.Router();
+
+router.get("/" , (req , res)=>{
+    res.send("석준아 나가라")
+})
+module.exports = router
---
+++ server/routes/index.js
@@ -0,0 +1,6 @@
+const router = express.Router();
+
+router.get("/" , (req , res)=>{
+    res.send("3조 박재형 박어진 박해준 이재윤")
+})
+module.exports = router
