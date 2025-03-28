f3e72239f12bce77aa34891ec1e7dc5a3a6b4828
bangme <qkrgkdbf10@gmail.com>
2025-03-28 14:58:42 +0900

    washer

---
+++ build/washer/CMakeFiles/washer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/washer.dir/washer.c.o"
+  "CMakeFiles/washer.dir/washer.c.o.d"
+  "washer"
+  "washer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/washer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
